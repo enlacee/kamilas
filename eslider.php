1148777fb8c1b523cad9176a3efe6bef002cd1fc
Anb <detops.tk@gmail.com>
2014-05-29 02:44:32 -0500

    Slider OK

--- eslider.php
+++ eslider.php
@@ -7,42 +7,39 @@
   
   <!--Eslider-->
   <div id="my-cute-slider" class="cute-slider" data-width="1040" data-height="392">
-  <ul data-type="slides">
-  
-      <?php foreach ($data as $key => $array) :
-            $data_trans3d = '';
-            $data_trans2d = '';
-            $data_delay = 4;
-            if ($key == 0) {
-                $data_trans3d = 'tr1,tr16';
-                $data_trans2d = 'tr2';
-            } elseif ($key == 1) {
-                $data_trans3d = 'tr2';
-                $data_trans2d = 'tr3';
-            } elseif ($key == 2) {
-                $data_trans3d = 'tr18';
-                $data_trans2d = 'tr4';
-                $data_delay = 6;
-            }
-      ?>
-        <li data-delay="<?php echo $data_delay ?>" data-trans3d="<?php echo $data_trans3d ?>" data-trans2d="<?php echo $data_trans2d ?>"> 
-          <img src="images/bannerCentral/<?php echo $array['image'] ?>" data-thumb="images/bannerCentral/<?php echo $array['image'] ?>"/>
-        </li>
-      <?php endforeach; ?>
-      
-</ul>
-    <ul data-type="controls">
-      <li data-type="captions"> </li>
-      <li data-type="link"> </li>
-      <li data-type="video"> </li>
-      <li data-type="slideinfo"> </li>
-      <li data-type="circletimer"> </li>
-      <li data-type="next"> </li>
-      <li data-type="previous"> </li>
-      <li data-type="slidecontrol"> </li>
-      <li data-type="bartimer"> </li>
+    <ul data-type="slides">  
+    <?php foreach ($data as $key => $array) : ?>
+        <?php if ($key == 0) : ?>
+          <li data-delay="4" data-trans3d="tr1,tr16" data-trans2d="tr2"> 
+            <img src="images/bannerCentral/<?php echo $array['image'] ?>" />
+          </li>
+        <?php elseif($key == 1) : ?>
+            <li data-delay="4" data-trans3d="tr1,tr16" data-trans2d="tr2"> 
+              <img src="cute-theme/blank.jpg" data-src="images/bannerCentral/<?php echo $array['image'] ?>"/>
+            </li>
+        <?php elseif($key == 1) : ?>
+            <li data-delay="4" data-trans3d="tr1,tr16" data-trans2d="tr2"> 
+              <img src="cute-theme/blank.jpg" data-src="images/bannerCentral/<?php echo $array['image'] ?>"/>
+            </li>
+        <?php else : ?>
+            <li data-delay="4" data-trans3d="tr1,tr16" data-trans2d="tr2"> 
+              <img src="cute-theme/blank.jpg" data-src="images/bannerCentral/<?php echo $array['image'] ?>"/>
+            </li>
+        <?php endif; ?>
+    <?php endforeach; ?>      
     </ul>
-  </div>
+    <ul data-type="controls">
+        <li data-type="captions"> </li>
+        <li data-type="link"> </li>
+        <li data-type="video"> </li>
+        <li data-type="slideinfo"> </li>
+        <li data-type="circletimer"> </li>
+        <li data-type="next"> </li>
+        <li data-type="previous"> </li>
+        <li data-type="slidecontrol"> </li>
+        <li data-type="bartimer"> </li>
+    </ul>
+    </div>
   <!--End Eslider--> 
   
 </div> 
